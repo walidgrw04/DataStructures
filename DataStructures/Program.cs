af503514e7d55dd4b71465788dc0527b6be7e29a
walidgraihim <bi29vw@student.sunderland.ac.uk>
2024-11-06 15:41:08 +0000

    Added 'someFloat' to output to 3dp

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
+
+float someFloat = 3.14159f;
+Console.WriteLine(someFloat.ToString("0.000"));
+Console.ReadKey();
